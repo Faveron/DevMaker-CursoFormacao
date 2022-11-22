fd707489f6b2ec1d906e03cdf21d7e176c20ec00
Wesley Faveron <w.faveron@aluno.ifsp.edu.br>
2022-11-21 21:19:25 -0300

    Criando funções/métodos para pegar/inserir dados do console ja tratados

---
+++ Módulo I - LÓGICA DE PROGRAMAÇÃO I/Aula 05 - 18-11-22/src/Teste.java
@@ -0,0 +1,138 @@
+/*
+    Tentando aprimorar a validação de entrada de número
+ */
+
+import java.util.Scanner;
+
+public class Teste
+{
+    static Scanner entrada = new Scanner(System.in);
+    static boolean verificador;
+
+    public static void main(String[] args)
+    {
+        double n;
+
+        do
+        {
+            System.out.print("\nDigite um inteiro: ");
+            n = getDouble();
+            if (n <= 0)
+            {
+                System.out.print("Digite um valor maior que zero!");
+                verificador = true;
+            } else verificador = false;
+        }while (verificador);
+
+        System.out.println();
+        System.out.println(n);
+    }
+
+    public static int getInt()
+    {
+        String numero;
+
+        do
+        {
+            try
+            {
+                numero = entrada.nextLine();
+                Integer.parseInt(numero);
+                verificador = false;
+            } catch (Exception e)
+            {
+                numero = "";
+                System.out.print("Entrada inválida!\nTente novamente: ");
+                verificador = true;
+            }
+        } while (verificador);
+
+        return Integer.parseInt(numero);
+    }
+
+    public static float getFloat()
+    {
+        String numero;
+
+        do
+        {
+            try
+            {
+                numero = entrada.nextLine();
+                Float.parseFloat(numero);
+                verificador = false;
+            } catch (Exception e)
+            {
+                numero = "";
+                System.out.print("Entrada inválida!\nTente novamente: ");
+                verificador = true;
+            }
+        } while (verificador);
+
+        return Float.parseFloat(numero);
+    }
+
+    public static double getDouble()
+    {
+        String numero;
+
+        do
+        {
+            try
+            {
+                numero = entrada.nextLine();
+                Double.parseDouble(numero);
+                verificador = false;
+            } catch (Exception e)
+            {
+                numero = "";
+                System.out.print("Entrada inválida!\nTente novamente: ");
+                verificador = true;
+            }
+        } while (verificador);
+
+        return Double.parseDouble(numero);
+    }
+
+    public static char getChar()
+    {
+        char caracter;
+
+        do
+        {
+            try
+            {
+                caracter = entrada.next().charAt(0);
+                verificador = false;
+            } catch (Exception e)
+            {
+                caracter = '*';
+                System.out.print("Entrada inválida!\nTente novamente: ");
+                verificador = true;
+            }
+        } while (verificador);
+
+        return caracter;
+    }
+
+    public static String getString()
+    {
+        String texto;
+
+        do
+        {
+            try
+            {
+                texto = entrada.nextLine();
+                verificador = false;
+            } catch (Exception e)
+            {
+                texto = "";
+                System.out.print("Entrada inválida!\nTente novamente: ");
+                verificador = true;
+            }
+        } while (verificador);
+
+        return texto;
+    }
+}
