fda5719cc9b516bbf41bd2de093f4cc1d12aa656
Wesley Faveron <w.faveron@aluno.ifsp.edu.br>
2022-11-22 22:36:58 -0300

    Commit final de aula

--- Módulo I - LÓGICA DE PROGRAMAÇÃO I/Aula 07 - 22-11-22/src/Main.java
+++ Módulo I - LÓGICA DE PROGRAMAÇÃO I/Aula 07 - 22-11-22/src/Main.java
@@ -1,7 +1,27 @@
+/*
+    Exemplo de como usar try catch finally
+ */
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int[] vetor = new int[10];
+        String s = null;
+
+        Scanner entrada = new Scanner(System.in);
+        try{
+            vetor[11] = 5;
+            s.toUpperCase();
+        }catch (NullPointerException e){
+            System.out.println("Erro acesso objeto nulo");
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Erro acesso index inválido");
+        }catch (Exception e){
+            System.out.println("Exceção genérica");
+        }finally{
+            entrada.close();
+        }
     }
 }
